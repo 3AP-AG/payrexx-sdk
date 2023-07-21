2c7426251dd0954bca7f8b754cd5d6aa7601cb2d
Veljko Tasic <veljko@3ap.ch>
2023-07-21 12:23:43 +0200

    refactor: rename src to lib

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import dts from "vite-plugin-dts";
 export default defineConfig({
   build: {
     lib: {
-      entry: resolve(__dirname, "src/index.ts"),
+      entry: resolve(__dirname, "lib/index.ts"),
       name: "payrexx-sdk",
       fileName: "payrexx-sdk",
     },
