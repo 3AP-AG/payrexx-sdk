7800bf6471971cdf52dcf27852bd4a015bb90901
Veljko Tasic <veljko@3ap.ch>
2023-07-28 15:15:58 +0200

    Update index.ts (#28)

--- lib/index.ts
+++ lib/index.ts
@@ -1,6 +1,7 @@
 import { PayrexxClient } from './api/client';
 import { Transaction } from './api/v1/types/transaction';
 import { PaylinkRequest, PaylinkResponse } from './api/v1/types/paylink';
+import { GatewayRequest, GatewayResponse } from './api/v1/types/gateway';
 
-export { PayrexxClient };
-export type { Transaction, PaylinkRequest, PaylinkResponse };
+export { PayrexxClient, PaylinkRequest, GatewayRequest };
+export type { Transaction, PaylinkResponse, GatewayResponse };
