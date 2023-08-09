3b56811694a6983c3079a314849de8ec6b204179
Veljko Tasic <veljko@3ap.ch>
2023-08-09 13:52:15 +0200

    Update payout-service.ts

--- lib/api/v1/service/payout-service.ts
+++ lib/api/v1/service/payout-service.ts
@@ -6,10 +6,19 @@ export class PayoutService extends Service {
     super(instance, apiSecret, 'Payout');
   }
 
+  /**
+   * @returns Returns all payouts
+   */
   async retrieveAll(): Promise<PayoutsResponse> {
     return this.get();
   }
 
+  /**
+   *
+   * @param id The id of payout to be retrieved
+   * @param withDetails Flag for fetching details
+   * @returns Returns a payout
+   */
   async retrieve(id: number, withDetails?: boolean): Promise<PayoutResponse> {
     return withDetails ? this.get(`${id}/details`) : this.get(`${id}`);
   }
